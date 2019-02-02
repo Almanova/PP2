0a92b7bb7537573ed964772b43fe63a1ae27ffdd
Almanova <almanovamadina@yahoo.com>
2019-02-03 04:01:13 +0600

    adding tasks from lab2

--- Week 2/Task3/Task3/Program.cs
+++ Week 2/Task3/Task3/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace Task3
+namespace Examples
 {
     class Program
     {
+        public static void PrintSpaces(int level)
+        {
+            for (int i = 0; i < level; i++)
+                Console.Write("     ");
+        }
+
+        public static void MainDirectory()
+        {
+            DirectoryInfo directory = new DirectoryInfo("/Users/Asus/Documents/Directory");
+            Console.WriteLine(directory.Name);
+            Console.WriteLine();
+        }
+
+        public static void Tree(DirectoryInfo dir, int level)
+        {
+            foreach (FileInfo f in dir.GetFiles())
+            {
+                PrintSpaces(level);
+                Console.WriteLine(f.Name);
+            }
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                PrintSpaces(level);
+                Console.WriteLine(d.Name);
+                Tree(d, level + 1);
+            }
+        }
+
         static void Main(string[] args)
         {
+            DirectoryInfo dir = new DirectoryInfo("/Users/Asus/Documents/Directory");
+            MainDirectory();
+            Tree(dir, 0);
+            Console.ReadKey();
         }
     }
 }
