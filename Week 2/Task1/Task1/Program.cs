dd8666a6cabebb8e6e0dee5c9b0498ddfa0a54f1
Almanova <almanovamadina@yahoo.com>
2019-02-03 01:00:32 +0600

    adding tasks from lab2

---
+++ Week 2/Task1/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        public static string ReverseString(string s) //writing a function that reverses a string
+        {
+            char[] arr = s.ToCharArray(); //adding every symbol of a string to char array
+            Array.Reverse(arr); //reversing an array
+            return new string(arr); //returning new reversed string
+        }
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt"); //reaching the "input.txt" file
+            string s = sr.ReadToEnd(); //reading a string from the file
+            string ss = ReverseString(s); //reversing the string
+            if (s == ss) //if the original string is the same as the reversed one, then it's palindrome
+                Console.WriteLine("YES"); //printing the result
+            else
+                Console.WriteLine("NO"); //printing the result
+            sr.Close(); //closing stream
+            Console.ReadKey(); //quiting the program be pressing any key (or otherwise it closes immediately)
+        }
+    }
+}
---
+++ Week 2/Task2/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        public static bool isPrime(string n) //writing a function to check for prime numbers
+        {
+            int a = int.Parse(n); //converting string into an integer
+            if (a <= 1) return false; //checking whether number is smaller or equal to 1(these are not considered primes)
+            else
+            {
+                for (int i = 2; i < a; i++) //running through all the numbers between 1 and n
+                    if (a % i == 0) //if there is other divider rather than 1 and n
+                        return false; //function returns false
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt"); //opening file "input.txt"
+            StreamWriter sw = new StreamWriter("output.txt"); //declaring the file for the output
+            string s = sr.ReadToEnd(); //reading all the numbers of an input as a string
+            string[] arr = s.Split(); //dividing them by space and adding each of them to an array
+            foreach(string a in arr) //running through th array
+            {
+                if (isPrime(a)) //using function to check for primes
+                    sw.Write(a + " "); //if number is prime, then printing it in "output.txt"
+            }
+            sr.Close(); //closing stream
+            sw.Close();
+        }
+    }
+}
