cdfdbea27e15c8d5d15bf8c7a84fc8a794b16897
Almanova <almanovamadina@yahoo.com>
2019-02-04 22:38:29 +0600

    adding tasks from lab2 and comments

--- Week 2/Task3/Task3/Program.cs
+++ Week 2/Task3/Task3/Program.cs
@@ -1,44 +1,44 @@
 ﻿using System;
 using System.IO;
 
-namespace Examples
+namespace Task3
 {
     class Program
     {
-        public static void PrintSpaces(int level)
+        public static void PrintSpaces(int level) //creating a method to iterate through the files and directories
         {
             for (int i = 0; i < level; i++)
                 Console.Write("     ");
         }
 
-        public static void MainDirectory()
+        public static void MainDirectory() //accessing main directory
         {
             DirectoryInfo directory = new DirectoryInfo("/Users/Asus/Documents/Directory");
-            Console.WriteLine(directory.Name);
+            Console.WriteLine(directory.Name); //printing it's name
             Console.WriteLine();
         }
 
-        public static void Tree(DirectoryInfo dir, int level)
+        public static void Tree(DirectoryInfo dir, int level) //creating a method to go through directories and files
         {
-            foreach (FileInfo f in dir.GetFiles())
+            foreach (FileInfo f in dir.GetFiles()) //going through every file
             {
-                PrintSpaces(level);
-                Console.WriteLine(f.Name);
+                PrintSpaces(level); //iterating
+                Console.WriteLine(f.Name); //printing file's name
             }
-            foreach (DirectoryInfo d in dir.GetDirectories())
+            foreach (DirectoryInfo d in dir.GetDirectories()) //going through every directory
             {
-                PrintSpaces(level);
-                Console.WriteLine(d.Name);
-                Tree(d, level + 1);
+                PrintSpaces(level); //iterating
+                Console.WriteLine(d.Name); //printing directory's name
+                Tree(d, level + 1); //recursively checking all subdirectories of a main directory
             }
         }
 
         static void Main(string[] args)
         {
-            DirectoryInfo dir = new DirectoryInfo("/Users/Asus/Documents/Directory");
-            MainDirectory();
+            DirectoryInfo dir = new DirectoryInfo("/Users/Asus/Documents/Directory"); //path to reach a directory
+            MainDirectory(); //calling functions
             Tree(dir, 0);
-            Console.ReadKey();
+            Console.ReadKey(); //quiting program by pressing any key (or otherwise it closes immediately)
         }
     }
 }
---
+++ Week 2/Task4/Task4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        public static void Create() //method to create a file within a directory
+        {
+            string filePath = @"C:\Users\Asus\Documents\Directory\File.txt"; //path where new file has to be created
+            File.Create(filePath).Close(); //creating a file
+        }
+         
+        public static void Copy() //method to copy file to another depository and delete the original one 
+        {
+            string fileName = "File.txt"; //declaring file name
+            string path = @"C:\Users\Asus\Documents\Directory"; //original path
+            string path1 = @"C:\Users\Asus\Documents\PP2\Week 2"; //target path
+
+            string sourceFile = Path.Combine(path, fileName); //combining directory and file paths
+            string destFile = Path.Combine(path1, fileName);
+
+            File.Copy(sourceFile, destFile, true); //coping file
+            File.Delete(sourceFile); //deleting file
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press UpArrow to create a file..."); //printing out text
+            ConsoleKeyInfo button = Console.ReadKey(); //adding the usage of keyboard
+            while (button.Key != ConsoleKey.Escape)
+            {
+                if (button.Key == ConsoleKey.UpArrow) //pressing UpArrow
+                {
+                    Create(); //calling method to create a file
+                    Console.WriteLine("File has been successfully created");
+                    Console.WriteLine();
+                    Console.WriteLine("Press RightArrow to copy this file and delete the original one...");
+                }
+
+                if (button.Key == ConsoleKey.RightArrow) //pressing RightArrow
+                {
+                    Copy(); //calling method to copy this file
+                    Console.WriteLine("File has been copied");
+                    Console.WriteLine();
+                    Console.WriteLine("Press Escape to quit...");
+                }
+
+                button = Console.ReadKey(); //quiting program by pressing Escape
+            }
+        }
+    }
+}
