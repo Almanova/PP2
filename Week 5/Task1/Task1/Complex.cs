d649faa3c94c5eb7ea5c70cf070adfc472703e38
Almanova <almanovamadina@yahoo.com>
2019-02-25 03:19:21 +0600

    adding tasks from lab4

---
+++ Week 5/Task1/Task1/Complex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Complex
+    {
+        public List<ComplexNumber> Numbers;
+
+        public Complex()
+        {
+            Numbers = new List<ComplexNumber>();
+        }
+
+        public Complex(ComplexNumber complexNumber)
+        {
+            Numbers = new List<ComplexNumber>();
+        }
+    }
+
+    public class ComplexNumber
+    {
+        public double Re;
+        public double Im;
+
+        public ComplexNumber(double Re, double Im)
+        {
+            this.Re = Re;
+            this.Im = Im;
+        }
+
+        public ComplexNumber() { }
+
+        public override string ToString()
+        {
+            return Re + " + " + Im + "i";
+        }
+    }
+}
---
+++ Week 5/Task1/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        public static void In()
+        {
+            Complex ComplexNumbers = new Complex();
+            ComplexNumber Number1 = new ComplexNumber(3, 5);
+            ComplexNumber Number2 = new ComplexNumber(4, 6);
+            ComplexNumbers.Numbers.Add(Number1);
+            ComplexNumbers.Numbers.Add(Number2);
+
+            FileStream fs = new FileStream("complex.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Complex));
+            xs.Serialize(fs, ComplexNumbers);
+            fs.Close();
+        }
+
+        public static void Out()
+        {
+            FileStream fs = new FileStream("complex.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Complex));
+            Complex ComplexNumbers = xs.Deserialize(fs) as Complex;
+
+            Console.WriteLine(ComplexNumbers.Numbers[0]);
+            Console.WriteLine(ComplexNumbers.Numbers[1]);
+
+            fs.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            In();
+            Out();
+
+            Console.ReadKey();
+        }
+    }
+}
