0ab238dafeb4b7249ce72b54a155bcffeab3e01e
Almanova <almanovamadina@yahoo.com>
2019-02-24 21:22:37 +0600

    adding tasks from lab4

---
+++ Week 5/Task2/Task2/Mark.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Mark
+    {
+        public List<MarkInfo> marks;
+
+        public Mark() 
+        {
+            marks = new List<MarkInfo>();
+        }
+
+        public Mark(MarkInfo markInfo) 
+        {
+            marks = new List<MarkInfo>();
+        }
+    }
+
+    public class MarkInfo 
+    {
+        public string StudentFirstName;
+        public string StudentLastName;
+        public string Subject;
+        public int Points;
+        public string Letter;
+
+        public MarkInfo(string StudentFirstName, string StudentLastName, string Subject, int Points)
+        {
+            this.StudentFirstName = StudentFirstName;
+            this.StudentLastName = StudentLastName;
+            this.Subject = Subject;
+            this.Points = Points;
+        }
+
+        public MarkInfo() { }
+
+        public void GetLetter() 
+        {
+            if (Points >= 95) Letter = "A";
+            else if (Points >= 90 && Points < 95) Letter = "A-";
+            else if (Points >= 85 && Points < 90) Letter = "B+";
+            else if (Points >= 80 && Points < 85) Letter = "B";
+            else if (Points >= 75 && Points < 80) Letter = "B-";
+            else if (Points >= 70 && Points < 75) Letter = "C+";
+            else if (Points >= 65 && Points < 70) Letter = "C";
+            else if (Points >= 60 && Points < 65) Letter = "C-";
+            else if (Points >= 55 && Points < 60) Letter = "D+";
+            else if (Points >= 50 && Points < 55) Letter = "D";
+            else if (Points < 50) Letter = "F";
+        }
+
+        public override string ToString()
+        {
+            return StudentFirstName + " " + StudentLastName + " - " + Subject + ": " + Letter;
+        }
+    }
+}
---
+++ Week 5/Task2/Task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        public static void In() 
+        {
+            Mark Marks = new Mark();
+            MarkInfo Mark1 = new MarkInfo("Madina", "Almanova", "Programming Principles 2", 99);
+            MarkInfo Mark2 = new MarkInfo("Fox", "Mulder", "History", 67);
+            MarkInfo Mark3 = new MarkInfo("Dana", "Scully", "Calculus 2", 86);
+            Mark1.GetLetter();
+            Mark2.GetLetter();
+            Mark3.GetLetter();
+            Marks.marks.Add(Mark1);
+            Marks.marks.Add(Mark2);
+            Marks.marks.Add(Mark3);
+
+            FileStream fs = new FileStream("mark.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Mark));
+            xs.Serialize(fs, Marks);
+            fs.Close();
+        }
+
+        public static void Out() 
+        {
+            FileStream fs = new FileStream("mark.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Mark));
+            Mark Marks = xs.Deserialize(fs) as Mark;
+
+            Console.WriteLine(Marks.marks[0]);
+            Console.WriteLine(Marks.marks[1]);
+            Console.WriteLine(Marks.marks[2]);
+
+            fs.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            In();
+            Out();
+
+            Console.ReadKey();
+        }
+    }
+}
